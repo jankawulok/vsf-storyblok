7fa9b93bc5585cc15af8768015f5dc3e6e9913eb
Jan Kawulok <jan@kawulok.com.pl>
2019-01-30 19:03:02 +0100

    module cleanup

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
 // This is VS module entry point.
 // Read more about modules: https://github.com/DivanteLtd/vue-storefront/blob/master/doc/api-modules/about-modules.md
 import { module } from './store'
-import { plugin } from './store/plugin'
 import { beforeRegistration } from './hooks/beforeRegistration'
-import { afterRegistration } from './hooks/afterRegistration'
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
-import { beforeEach } from './router/beforeEach'
-import { afterEach } from './router/afterEach'
 import { initCacheStorage } from '@vue-storefront/core/helpers/initCacheStorage'
 // This key will be used for creating extension keys in vuex and other key-based plugins. 
 // In case of conflicting keys across modules they'll be merged in favor of the least recently registered one
@@ -17,9 +13,7 @@ export const cacheStorage = initCacheStorage(KEY)
 // Put everything that should extend the base app here so it can be later registered as VS module
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
-  store: { modules: [{ key: KEY, module }], plugin },
-  beforeRegistration,
-  afterRegistration,
-  router: { beforeEach, afterEach }
+  store: { modules: [{ key: KEY, module }] },
+  beforeRegistration
 }
 export const Storyblok = new VueStorefrontModule(moduleConfig)
